ec955b17ebabb9647cf0fba7c8f970aece6f3942
Christian Kniep <christian@qnib.org>
2015-12-28 20:51:49 +0100

    iterate on hpcg.sh

--- opt/qnib/jobscripts/hpcg.sh
+++ opt/qnib/jobscripts/hpcg.sh
@@ -6,22 +6,25 @@
 
 JOBID=${SLURM_JOBID}
 NLIST=${SLURM_NODELIST}
+JOB_TIME=${1-120}
+JOB_SIZE=${2-104}
 START_TIME=$(date +%s)
+CMD=/opt/hpcg-3.0/Linux_MPI/bin/xhpcg
 NODES=$(python -c "from ClusterShell.NodeSet import NodeSet;print ' '.join(NodeSet('${SLURM_NODELIST}'))")
 logger -t slurm_${JOBID}  "Job ${JOBID} starts."
 echo "####################################################"
 echo "################ JOBRUN ############################"
 echo "####################################################"
-logger -t slurm_${JOBID}  "mpirun -q ${CMD}"
+logger -t slurm_${JOBID}  "mpirun -q ${CMD} TIME:${JOB_TIME} SIZE:${JOB_SIZE}"
 mkdir -p /scratch/hpcg/${JOBID}
 cat << \EOF > /scratch/hpcg/${JOBID}/hpcg.dat
 HPCG benchmark input file
 Sandia National Laboratories; University of Tennessee, Knoxville
-104 104 104
-120
+${JOB_SIZE} ${JOB_SIZE} ${JOB_SIZE}
+${JOB_TIME}
 EOF
 cd /scratch/hpcg/${JOBID}/
-mpirun -q /opt/hpcg-3.0/Linux_MPI/bin/xhpcg
+mpirun -q ${CMD}
 echo "####################################################"
 echo "################ \JOBRUN ############################"
 echo "####################################################"
