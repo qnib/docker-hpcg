05f19930e7a853741a503faa980758cb1082b595
Christian Kniep <christian@qnib.org>
2015-12-28 22:03:30 +0100

    bugfix jobscript v2

--- opt/qnib/jobscripts/hpcg.sh
+++ opt/qnib/jobscripts/hpcg.sh
@@ -18,9 +18,9 @@ echo "####################################################"
 logger -t slurm_${JOBID}  "mpirun -q ${CMD} TIME:${JOB_TIME} SIZE:${JOB_SIZE}"
 mkdir -p /scratch/hpcg/${JOBID}
 echo "HPCG benchmark input file" > /scratch/hpcg/${JOBID}/hpcg.dat
-echo "Sandia National Laboratories; University of Tennessee, Knoxville" > /scratch/hpcg/${JOBID}/hpcg.dat
-echo "${JOB_SIZE} ${JOB_SIZE} ${JOB_SIZE}" > /scratch/hpcg/${JOBID}/hpcg.dat
-echo "${JOB_TIME}" > /scratch/hpcg/${JOBID}/hpcg.dat
+echo "Sandia National Laboratories; University of Tennessee, Knoxville" >> /scratch/hpcg/${JOBID}/hpcg.dat
+echo "${JOB_SIZE} ${JOB_SIZE} ${JOB_SIZE}" >> /scratch/hpcg/${JOBID}/hpcg.dat
+echo "${JOB_TIME}" >> /scratch/hpcg/${JOBID}/hpcg.dat
 cd /scratch/hpcg/${JOBID}/
 mpirun -q ${CMD}
 echo "####################################################"
